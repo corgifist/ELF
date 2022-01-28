53a25b9e7f9b2fa432ad02a5b8338d1ef2d73787
corgifist <86985149+corgifist@users.noreply.github.com>
2022-01-28 21:48:10 +0300

    Ability to switch graphics pipeline (legacy / modern) + custom sky

--- elf.py
+++ elf.py
@@ -10,10 +10,13 @@ from rpcore.util.movement_controller import MovementController
 class ELF_RUNTIME(ShowBase):
     def __init__(self):
         global ELF_PIPELINE
-        # super().__init__()
-        ELF_PIPELINE = RenderPipeline()
-        ELF_PIPELINE.set_loading_screen_image("assets/splash.jpg")
-        ELF_PIPELINE.create(self)
+        if not ELF_PIPELINE_FLAG:
+            super().__init__()
+        else:
+            ELF_PIPELINE = RenderPipeline()
+            ELF_PIPELINE.set_loading_screen_image("assets/splash.jpg")
+            ELF_PIPELINE.create(self)
+            ELF_PIPELINE.add_environment_probe()
 
     def update(self, task):
         pass
@@ -25,10 +28,19 @@ ELF_SHADERS = {}
 ELF_TEXTURES = {}
 ELF_PIPELINE: RenderPipeline = None
 ELF_FILTERS: CommonFilters = None
+ELF_PIPELINE_FLAG = True
 
 
 def elf_enable_mouse():
     ELF_SHOW_BASE.enableMouse()
+
+
+def elf_switch_to_legacy_pipeline(flag):
+    global ELF_PIPELINE_FLAG
+    if ELF_SHOW_BASE != None:
+        print("Can't switch to legacy pipeline, because ShowBase is ready to use.\nPlease use "
+              "'elf_switch_to_legacy_pipeline' before initializing ShowBase!")
+    ELF_PIPELINE_FLAG = not flag
 
 
 def elf_init_show_base():
@@ -70,15 +82,39 @@ def elf_show_scene_graph_analyzer_meter(analyzer):
     loadPrcFileData('', 'show-scene-graph-analyzer-meter ' + str(int(analyzer)))
 
 
+def elf_fullscreen(mode):
+    if ELF_SHOW_BASE != None:
+        print("ELF: failed to change fullscreen because this function is special, and needs to be called "
+              "before "
+              "'elf_init_show_base'")
+    loadPrcFileData('', "fullscreen " + str(mode))
+
+
+def elf_render_resolution(w, h):
+    if ELF_SHOW_BASE != None:
+        print("ELF: failed to change render-res because this function is special, and needs to be called "
+              "before "
+              "'elf_init_show_base'")
+
+    loadPrcFileData('', f'win-size {w} {h}')
+
+
+def elf_camera_fov(fov):
+    ELF_SHOW_BASE.camLens.setFov(fov)
+
+
 def elf_use_assimp():
     if ELF_SHOW_BASE != None:
         print("ELF: failed to change load-file-type because this function is special, and needs to be called "
               "before "
               "'elf_init_show_base'")
-    #loadPrcFileData('', 'load-file-type p3assimp')
+    loadPrcFileData('', 'load-file-type p3assimp')
 
 
 def elf_parse_glsl(vertex, fragment):
+    if ELF_PIPELINE_FLAG:
+        print("Can't parse and use raw GLSL shaders, because ELF is using RenderPipeline")
+        return -1
     shader = Shader.load(Shader.SL_GLSL, vertex=vertex, fragment=fragment)
     id = elf_gen_id()
     ELF_SHADERS[id] = shader
@@ -86,17 +122,25 @@ def elf_parse_glsl(vertex, fragment):
 
 
 def elf_attach_shader_to_model(model_id, shader_id):
+    if ELF_PIPELINE_FLAG:
+        print("Can't attach raw GLSL shader, because engine using RenderPipeline")
     ELF_OBJECTS[model_id].setShader(ELF_SHADERS[shader_id])
     return model_id
 
 
 def elf_attach_pipeline_effect(model_id, effect, options=None):
+    if not ELF_PIPELINE_FLAG:
+        print("Can't attach pipeline effect when it is disabled!")
+        return
     if options is None:
         options = {}
     ELF_PIPELINE.set_effect(ELF_OBJECTS[model_id], effect, options)
 
 
 def elf_set_in_glsl(model_id, uniform_name, value):
+    if not ELF_PIPELINE_FLAG:
+        print("Can't attach pipeline effect when it is disabled!")
+        return
     ELF_OBJECTS[model_id].setShaderInput(uniform_name, value)
     return model_id
 
@@ -113,22 +157,19 @@ def elf_loader():
     return ELF_SHOW_BASE.loader
 
 
-def elf_draw(path, position=(0, 0, 0), scale=(1.0, 1.0, 1.0), rotation=(0, 0, 0)):
+def elf_draw(path, position=(0, 0, 0), scale=(1.0, 1.0, 1.0), rotation=(0, 0, 0), set_two_sided=False):
     id = elf_gen_id()
     ELF_OBJECTS[id] = elf_loader().loadModel(path)
     ELF_OBJECTS[id].setPos(position[0], position[1], position[2])
     ELF_OBJECTS[id].setScale(scale[0], scale[1], scale[2])
     ELF_OBJECTS[id].setHpr(rotation[0], rotation[1], rotation[2])
-    material = Material()
-    material.setBaseColor((1, 0, 0, 1))
-    material.setRoughness(0)
-    material.setEmission((0, 0, 0, 0))
-    material.setDiffuse(0)
-    material.setShininess(0)
-    material.setAmbient((0, 0, 0, 0))
-    ELF_OBJECTS[id].setMaterial(material)
+    ELF_OBJECTS[id].setTwoSided(set_two_sided)
     ELF_OBJECTS[id].reparentTo(ELF_SHOW_BASE.render)
     return id
+
+
+def elf_set_two_sided(obj, sides):
+    ELF_OBJECTS[obj].setTwoSided(sides)
 
 
 def elf_bind_texture(path):
--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from direct.showbase.ShowBaseGlobal import globalClock
 
 import elf
 
+elf.elf_render_resolution(1280, 720)
+elf.elf_fullscreen(True)
 elf.elf_show_fps(True)
+elf.elf_switch_to_legacy_pipeline(False)
 elf.elf_init_show_base()
 elf.elf_prefab_noclip()
+elf.elf_camera_fov(90)
 clouds = elf.elf_draw("models/plane.obj", scale=(4, 4, 4), position=(0, 10, 0), rotation=(-90, 0, 0))
 gradient = elf.elf_draw("models/box", scale=(2, 2, 2), position=(15, 10, 0))
+sky_texture = elf.elf_bind_texture("assets/clouds.png")
+sky = elf.elf_draw("models/sky.obj", scale=(1000, 1000, 1000), rotation=(0, 90, 0))
+elf.elf_attach_texture_to_id(sky, sky_texture)
+elf.elf_attach_pipeline_effect(sky, "effects/skybox.yaml")
+elf.elf_set_two_sided(sky, True)
 elf.elf_attach_pipeline_effect(gradient, "shaders/easy_gradient.yaml")
 elf.elf_attach_pipeline_effect(clouds, "shaders/shadertoy_fog.yaml")
 
